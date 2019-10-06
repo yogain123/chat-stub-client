a3af85d180ae20998d42be20855041251fdd9cd4
yogain19 <yogendrasaxena56@gmail.com>
2019-10-06 17:48:29 +0530

    settings

---
+++ build/precache-manifest.5fe9a3c7ab87ac58865e00c6cfd6572f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c05570d0bbf549c649a7c6e2e441ae7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54bc41c2cdf01bb4f249",
+    "url": "/static/css/main.5d41ca5f.chunk.css"
+  },
+  {
+    "revision": "1c40536bf09271c3dda7",
+    "url": "/static/js/2.42b94587.chunk.js"
+  },
+  {
+    "revision": "54bc41c2cdf01bb4f249",
+    "url": "/static/js/main.e92d0fb3.chunk.js"
+  },
+  {
+    "revision": "e7e38dc9151a25df6bb5",
+    "url": "/static/js/runtime~main.59838607.js"
+  }
+]);
